5949d25c44adb8d945a73599c8655d97a3def0d7
abohussein <111657759+bashamahmoud@users.noreply.github.com>
2022-11-26 13:40:42 +0200

    change to parent register

--- models/user_parent.js
+++ models/user_parent.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const User = mongoose.model('User', new mongoose.Schema({
+const User_parent = mongoose.model('User_parent', new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -23,15 +23,15 @@ const User = mongoose.model('User', new mongoose.Schema({
     }
 }));
 
-function validateUser(user) {
+function validateUser(user_parent) {
 const schema =
 Joi.object({ name: Joi.string() .min(3) .required(),
                             email: Joi.string() .min(5) .required() .email(),
                             password: Joi.string() .min(5) .required() });
 
-const validation = schema.validate(user);
+const validation = schema.validate(user_parent);
 return validation;
 }
 
-exports.User = User;
+exports.User_parent = User_parent;
 exports.validate = validateUser;
--- routes/users_parent.js
+++ routes/users_parent.js
@@ -1,4 +1,4 @@
-const { User, validate } = require('../models/user');
+const { User_parent, validate } = require('../models/user_parent');
 const express = require('express');
 const router = express.Router();
 
@@ -10,18 +10,18 @@ router.post('/', async (req, res) => {
     }
 
     // Check if this user already exisits
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
+    let user_parent = await User_parent.findOne({ email: req.body.email });
+    if (user_parent) {
         return res.status(400).send('That user already exisits!');
     } else {
         // Insert the new user if they do not exist yet
-        user = new User({
+        user_parent = new User_parent({
             name: req.body.name,
             email: req.body.email,
             password: req.body.password
         });
-        await user.save();
-        res.send(user);
+        await user_parent.save();
+        res.send(user_parent);
     }
 });
 
