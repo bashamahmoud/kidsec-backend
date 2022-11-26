3fac49ca700766f5f5c9578c89860406d6345889
abohussein <111657759+bashamahmoud@users.noreply.github.com>
2022-11-26 16:46:00 +0200

    auth

---
+++ routes/auth_child.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
+const { User_child } = require('./../models/user_child');
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+
+});
+
+module.exports = router;
