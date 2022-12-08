547475343e6662351bbc598f5b85b54509de47aa
abohussein <111657759+bashamahmoud@users.noreply.github.com>
2022-12-08 20:28:42 +0200

    change index out app dir

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const users_parent = require("./../routes/users_parent");
-const users_child = require("./../routes/users_child");
-const auth_parent = require("./../routes/auth_parent");
-const auth_child = require("./../routes/auth_child");
+const users_parent = require("./routes/users_parent");
+const users_child = require("./routes/users_child");
+const auth_parent = require("./routes/auth_parent");
+const auth_child = require("./routes/auth_child");
 const express = require("express");
 const app = express();
 mongoose
@@ -13,10 +13,15 @@ mongoose
   )
   .then(() => console.log("Now connected to MongoDB!"))
   .catch((err) => console.error("Something went wrong", err));
-app.use(express.json());
+
+
+
+app.use(express.json()); // to enable express from understanding json
+
 app.use("/api/users_parent", users_parent);
 app.use("/api/users_child", users_child);
 app.use("/api/auth_parent", auth_parent);
 app.use("/api/auth_child", auth_child);
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
