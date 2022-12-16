867381cf5c35568e68ffbab89598c7219269332e
Tariq Sabri <57255961+HuangLinBao@users.noreply.github.com>
2022-12-16 18:27:43 +0200

    changed response in name.js

--- routes/name.js
+++ routes/name.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const { User} = require("../models/user");
+const { User } = require("../models/user");
+const EJSON = require("ejson");
+const BSON = require("bson");
 const router = express.Router();
 
-
-router.post('/', async (req, res)=> {
-let Name= await User.findOne({email: req.body.email});
-if (!Name) {
-    return res.status(400).send("Incorrect email");
-  }
-  else{
-    res.send(Name.name);
-  }
-
+router.get("/", async(req, res) => {
+    let Name = await User.findOne({ email: req.body.email });
+    if (!Name) {
+        return res.status(400).send("Incorrect email");
+    } else {
+        res.send(
+            EJSON.toJSONValue(
+                "email: " + Name.email + "\nname: " + Name.name + "\nid: " + Name.id
+            )
+        );
+    }
 });
 module.exports = router;
-
-
