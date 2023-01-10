6d47d269669545ec74326f74235d41f804a6a725
Tariq Sabri <57255961+HuangLinBao@users.noreply.github.com>
2023-01-10 18:10:22 +0200

    name fetch added

--- routes/name.js
+++ routes/name.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { User } = require("../models/user");
 const router = express.Router();
 
-router.get("/:email", async(req, res) => {
-    let Name = await User.find({ email: req.params.email }, { email: 1, name: 1, id: 1 });
+router.get("/:fetch", async(req, res) => {
+    let Name = await User.findOne({ $or: [{ email: req.params.fetch }, { name: req.params.fetch }] }, { email: 1, name: 1, id: 1 });
     if (!Name) {
         return res.status(400).send("Incorrect email");
     } else {
