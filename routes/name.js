1fafd14ddd08e24ad0b5b075bf4c01b24226bece
Tariq Sabri <57255961+HuangLinBao@users.noreply.github.com>
2023-01-23 06:22:14 +0200

    ee

--- routes/name.js
+++ routes/name.js
@@ -3,34 +3,28 @@ const { User } = require("../models/user");
 const mongodb = require("mongodb");
 const router = express.Router();
 
-router.get("/:fetch", async (req, res) => {
-  let Name = await User.findOne(
-    {
-      $or: [{ email: req.params.fetch }, { name: req.params.fetch }],
-    },
-    { id: 1 }
-  );
-  if (!Name) {
-    return res.status(400).send("Incorrect email");
-  } else {
-    res.send(Name);
-  }
+router.get("/:fetch", async(req, res) => {
+    let Name = await User.findOne({
+        $or: [{ email: req.params.fetch }, { name: req.params.fetch }],
+    }, { id: 1, name: 1 });
+    if (!Name) {
+        return res.status(400).send("Incorrect email");
+    } else {
+        res.send(Name);
+    }
 });
 
-router.get("/byID/:id", async (req, res) => {
-  if (!mongodb.ObjectId.isValid(req.params.id)) {
-    return res.status(400).send("Invalid ObjectId");
-  }
-  let Name = await User.findOne(
-    {
-      $or: [{ _id: mongodb.ObjectId(req.params.id) }],
-    },
-    { email: 1, name: 1 }
-  );
-  if (!Name) {
-    return res.status(400).send("Incorrect email");
-  } else {
-    res.send(Name);
-  }
+router.get("/byID/:id", async(req, res) => {
+    if (!mongodb.ObjectId.isValid(req.params.id)) {
+        return res.status(400).send("Invalid ObjectId");
+    }
+    let Name = await User.findOne({
+        $or: [{ _id: mongodb.ObjectId(req.params.id) }],
+    }, { email: 1, name: 1 });
+    if (!Name) {
+        return res.status(400).send("Incorrect email");
+    } else {
+        res.send(Name);
+    }
 });
 module.exports = router;
